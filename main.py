3aba173c6fc4fc8d675c3ffffa45a36dfbadad9e
kblc9l <97379286+kblc9l@users.noreply.github.com>
2024-02-29 09:23:09 +0400

    По каютам!

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ def list_prof(typeL: str):
     return render_template('prof_list.html', title='Список профессий для полёта на Марс', profs=profs, typeL=typeL, )
 
 
-@app.route('/')
-def practice():
-    return render_template('my_first_template.html')
+# @app.route('/')
+# def practice():
+#     return render_template('my_first_template.html')
 
+
+@app.route('/distribution')
+def distribution():
+    l = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандерс', 'Шон Бил']
+    return render_template('distribution.html', title='По каютам!', l=l)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080)
